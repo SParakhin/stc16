5f97720895d9858cb734606e49157c70fabec14b
SParakhin <ormts@mail.ru>
2019-07-13 13:59:41 +0300

    Добавлены методы в контроллер регистрации/удаления/редактирования пользователя и соответствующие формы.Тоже для адресов доставки. Исправлены замечания по jsp страницам.

--- src/main/java/ru/innopolis/stc16/innobazaar/controller/UserController.java
+++ src/main/java/ru/innopolis/stc16/innobazaar/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.innopolis.stc16.innobazaar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
