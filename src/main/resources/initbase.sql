9c906c874c3402f8dee99a490947bf56b765daae
Sergey Parakhin <ormts@mail.ru>
2019-07-22 12:28:37 +0300

    Security,профиль пользователя.

--- src/main/resources/initbase.sql
+++ src/main/resources/initbase.sql
@@ -1,18 +1,18 @@
-create schema innobazaar;
+-- create schema innobazaar;
+--
+-- create user student with encrypted password 'student123';
+-- grant all privileges on database innobazaar to student;
+--
+-- CREATE DATABASE innobazaar
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'ru_RU.utf-8'
+--     LC_CTYPE = 'ru_RU.utf-8'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1;
 
-create user student with encrypted password 'student123';
-grant all privileges on database innobazaar to student;
-
-CREATE DATABASE innobazaar
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'ru_RU.utf-8'
-    LC_CTYPE = 'ru_RU.utf-8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
------------------------------------------------------
+# -----------------------------------------------------
 -- Настройка таблиц для безопасности
 
 INSERT INTO public.role (name)
