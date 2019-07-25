ccd43d21d83cf8cea8239a65513708530112dff6
Tuchnyak <georgii.o.sv@gmail.com>
2019-07-25 20:59:59 +0300

    feature245: подготовлен перехватчик для логирования

--- src/main/java/ru/innopolis/stc16/innobazaar/config/WebMvcConfig.java
+++ src/main/java/ru/innopolis/stc16/innobazaar/config/WebMvcConfig.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import ru.innopolis.stc16.innobazaar.interceptor.LogInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -18,6 +16,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Autowired
     private ApplicationContext applicationContext;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()).addPathPatterns("/**");
+    }
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
