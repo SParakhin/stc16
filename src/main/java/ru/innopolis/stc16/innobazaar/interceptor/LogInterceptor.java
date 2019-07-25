a8af2215e8c3f7de005e11c0beeb09a63e86ef41
Tuchnyak <georgii.o.sv@gmail.com>
2019-07-25 23:22:51 +0300

    feature245: тестовая запись информации из запроса

--- src/main/java/ru/innopolis/stc16/innobazaar/interceptor/LogInterceptor.java
+++ src/main/java/ru/innopolis/stc16/innobazaar/interceptor/LogInterceptor.java
@@ -1,5 +1,9 @@
 package ru.innopolis.stc16.innobazaar.interceptor;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -12,19 +16,36 @@ import java.util.Map;
 
 public class LogInterceptor implements HandlerInterceptor {
 
+    private static final Logger LOGGER = Logger.getRootLogger();
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpServletRequest cachedWrappedRequest = new ContentCachingRequestWrapper(request);
         Map<String, String[]> requestParameters = cachedWrappedRequest.getParameterMap();
 
-        Enumeration<String> enumeration = cachedWrappedRequest.getSession().getAttributeNames();
-        while (enumeration.hasMoreElements()) {
-            String s = enumeration.nextElement();
-            System.out.println("================>>>>>> : " + s);
-        }
+        System.out.println("=================================================");
+        System.out.println("=================================================");
+        System.out.println("=================================================");
+        LOGGER.info("=====>>>>> method: " + cachedWrappedRequest.getMethod());
+        LOGGER.info("=====>>>>> context path: " + cachedWrappedRequest.getContextPath());
+        LOGGER.info("=====>>>>> path info: " + cachedWrappedRequest.getPathInfo());
+        LOGGER.info("=====>>>>> path translated: " + cachedWrappedRequest.getPathTranslated());
+        LOGGER.info("=====>>>>> query string: " + cachedWrappedRequest.getQueryString());
+        LOGGER.info("=====>>>>> request URI: " + cachedWrappedRequest.getRequestURI());
+        LOGGER.info("=====>>>>> servlet path: " + cachedWrappedRequest.getServletPath());
+        LOGGER.info("=====>>>>> http servlet mapping: " + cachedWrappedRequest.getHttpServletMapping());
 
-        for (Map.Entry<String, String[]> reqPar : requestParameters.entrySet()) {
-            System.out.println("======>>>>>> " + reqPar.getKey() + " : " + Arrays.toString(reqPar.getValue()));
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        LOGGER.info("=====>>>>> user: " + ((principal instanceof UserDetails) ? ((UserDetails) principal).getUsername() : principal));
+        LOGGER.info("=====>>>>> authorities: " + SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+
+        if (!requestParameters.isEmpty()) {
+            for (Map.Entry<String, String[]> par : requestParameters.entrySet()) {
+                if (!"_csrf".equals(par.getKey())) {
+                    LOGGER.info("===>>> post parameter " + par.getKey() + " : " + Arrays.toString(par.getValue()));
+                }
+            }
         }
 
         return true;
