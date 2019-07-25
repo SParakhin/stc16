e8cb343e7f257bcdcf3b42d539bbbec048e8ec9d
Tuchnyak <georgii.o.sv@gmail.com>
2019-07-25 21:25:43 +0300

    feature245: тестовое получение данных из запроса

--- src/main/java/ru/innopolis/stc16/innobazaar/interceptor/LogInterceptor.java
+++ src/main/java/ru/innopolis/stc16/innobazaar/interceptor/LogInterceptor.java
@@ -2,15 +2,32 @@ package ru.innopolis.stc16.innobazaar.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
 
 public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+        HttpServletRequest cachedWrappedRequest = new ContentCachingRequestWrapper(request);
+        Map<String, String[]> requestParameters = cachedWrappedRequest.getParameterMap();
+
+        Enumeration<String> enumeration = cachedWrappedRequest.getSession().getAttributeNames();
+        while (enumeration.hasMoreElements()) {
+            String s = enumeration.nextElement();
+            System.out.println("================>>>>>> : " + s);
+        }
+
+        for (Map.Entry<String, String[]> reqPar : requestParameters.entrySet()) {
+            System.out.println("======>>>>>> " + reqPar.getKey() + " : " + Arrays.toString(reqPar.getValue()));
+        }
+
+        return true;
     }
 
     @Override
