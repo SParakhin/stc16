95582e99aa8f8ce1e48f2ec2197fe60e2398ebbc
SParakhin <ormts@mail.ru>
2019-08-04 19:25:11 +0300

    Merge branch 'ergashevRD_task159_basket' of https://github.com/iskhakovaks/stc16 into iteration5-1
    # Conflicts:
    #	src/main/java/ru/innopolis/stc16/innobazaar/controller/MerchandiseController.java
    #	src/main/java/ru/innopolis/stc16/innobazaar/entity/User.java

--- src/main/java/ru/innopolis/stc16/innobazaar/controller/BasketController.java
+++ src/main/java/ru/innopolis/stc16/innobazaar/controller/BasketController.java
@@ -3,23 +3,31 @@ package ru.innopolis.stc16.innobazaar.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.innopolis.stc16.innobazaar.entity.Basket;
 import ru.innopolis.stc16.innobazaar.entity.Merchandise;
 import ru.innopolis.stc16.innobazaar.service.BasketService;
+import ru.innopolis.stc16.innobazaar.service.MerchandiseService;
 
+import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class BasketController {
     private final BasketService basketService;
+    private final HttpSession session;
+    private final MerchandiseService merchandiseService;
 
     @Autowired
-    public BasketController(BasketService basketService) {
+    public BasketController(BasketService basketService, HttpSession session, MerchandiseService merchandiseService) {
         this.basketService = basketService;
+        this.session = session;
+        this.merchandiseService = merchandiseService;
     }
 
 
@@ -33,5 +41,36 @@ public class BasketController {
         model.addAttribute("totalSum", totalSum);
         return "basket";
     }
+
+    /**
+     * Метод добавления товара в корзину. Сохранение корзины в сессии
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/addBasket")
+    public String addBasket(@RequestParam Long id) {
+        List<Merchandise> basket = (List<Merchandise>) session.getAttribute("basket");
+        Merchandise merchandise = merchandiseService.getMerchandise(id);
+        if (basket == null) {
+            basket = new ArrayList<>();
+        } else {
+            basket.add(merchandise);
+        }
+        session.setAttribute("basket", basket);
+        return "redirect:/";
+    }
+
+    /**
+     * Страница корзины
+     */
+    @GetMapping("/basket")
+    public String showBasket(Model model) {
+        //TODO Если корзина пустая, добавить атрибут запроса newBasket и в jsp вывод сообщения
+        List<Merchandise> basket = (List<Merchandise>) session.getAttribute("basket");
+        model.addAttribute("basket",basket);
+        return "basket";
+    }
+
 
 }
