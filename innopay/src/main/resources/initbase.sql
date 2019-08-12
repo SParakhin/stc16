d0df16f22d08a722d5ef3a97b5de66b087852958
Sergey Parakhin <ormts@mail.ru>
2019-08-12 16:16:56 +0300

    Ветка Дмитрия

--- innopay/src/main/resources/initbase.sql
+++ innopay/src/main/resources/initbase.sql
@@ -2,11 +2,11 @@ CREATE DATABASE innopay
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'ru_RU.utf-8'
-    LC_CTYPE = 'ru_RU.utf-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-grant all privileges on database innopay to student;
+#     LC_COLLATE = 'ru_RU.utf-8'
+#     LC_CTYPE = 'ru_RU.utf-8'
+grant all privileges on database innopay to postgres;
 
 INSERT INTO store (id, name, secret_key) VALUES (1, 'innobazaar', '4728ac9e-4618-44ea-8a55-c915c1b33b43');
